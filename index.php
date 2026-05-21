48eea08bbe1447add5fe726955d4020df404ca2c
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-05-21 12:25:21 -0300

    add - comecei o php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db = "sistema_simples";
+
+$conn = new mysqli($host,$user,$pass,$db);
+
+
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">

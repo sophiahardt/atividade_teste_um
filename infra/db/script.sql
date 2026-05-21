e8d52f337f234e61b6b43939b73d05e65e840e4d
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-05-21 12:08:16 -0300

    add - adicionei um banco de dados

---
+++ infra/db/script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE sistema_simples;
+
+USE sistema_simples;
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY_KEY,
+    usuario VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+INSERT INTO usuario(usuario,senha) VALUES ('admin','123');

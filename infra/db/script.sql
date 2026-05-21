df043e7c1dc5417aa4eba00c8c9a8f225f9b2018
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-05-21 12:15:07 -0300

    fix - corrigido erro de sintaxe na definição da tabela usuario

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -3,7 +3,7 @@ CREATE DATABASE sistema_simples;
 USE sistema_simples;
 
 CREATE TABLE usuario (
-    id INT AUTO_INCREMENT PRIMARY_KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL
 );

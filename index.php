85bb886b837eb57cee5885ef169d16e29bd892ef
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-05-21 11:51:28 -0300

    add - commit inicial

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login com o Banco</title>
+</head>
+<body>
+    <h2>
+        Login com PHP
+    </h2>
+
+    <form method="POST">
+        <label for="usuario">Usuario</label>
+        <input type="text" name="usuario">
+
+        <br><br>
+
+        <label for="senha">Senha</label>
+        <input type="password" name="senha">
+    </form>
+</body>
+</html>

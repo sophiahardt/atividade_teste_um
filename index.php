380de2108850aa58a91677bdee864d17002a4289
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-05-21 12:47:49 -0300

    add - adicionei uma condição pra caso a coneção der erro ou der tudo certo

--- index.php
+++ index.php
@@ -8,6 +8,13 @@ $db = "sistema_simples";
 
 $conn = new mysqli($host,$user,$pass,$db);
 
+if($conn->connect_error){
+    die("Erro na conexão!");
+}else{
+    // echo "<p> Banco: OK! </p>";
+    echo "<script> console.log('Banco: OK!') </script>";
+};
+
 
 ?>
 
